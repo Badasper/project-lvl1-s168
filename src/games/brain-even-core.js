d7af3f8d2484e8836e1ec1b4e12402569094a029
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2017-11-25 21:05:03 +0700

    refactor brain-even

--- src/games/brain-even-core.js
+++ src/games/brain-even-core.js
@@ -1,9 +1,8 @@
 import { getRandom, playGame, getRequest } from '..';
 
 export default () => playGame(3, () => {
-  const isEven = num => num % 2 === 0;
   const question = getRandom(1, 100);
-  const answer = isEven(question) ? 'yes' : 'no';
+  const answer = question % 2 === 0 ? 'yes' : 'no';
   const rule = 'Answer "yes" if number even otherwise answer "no".';
 
   return message => getRequest(message, question, answer, rule);
