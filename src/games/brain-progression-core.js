2980ce308ef0585c6bc530efefdefeaa129ff404
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2017-11-26 17:54:48 +0700

    refactor brain-progression

--- src/games/brain-progression-core.js
+++ src/games/brain-progression-core.js
@@ -1,6 +1,20 @@
 import { playGame, cons } from '..';
 import { getRandom, getItemOfProgression } from '../utilites';
 
+const getProgression = (lenProgression, start, step, idx) => {
+  let secuence = '';
+  let count = 0;
+  while (count < lenProgression) {
+    if (count === idx) {
+      secuence = `${secuence}.. `;
+    } else {
+      secuence = `${secuence}${getItemOfProgression(start, step, count)} `;
+    }
+    count += 1;
+  }
+  return secuence;
+};
+
 export default () => {
   const rule = 'What number is missing in this progression?';
   return playGame(rule, () => {
@@ -8,16 +22,7 @@ export default () => {
     const start = getRandom(1, 50);
     const idx = getRandom(0, 9);
     const lenProgression = 10;
-    let question = '';
-    let count = 0;
-    while (count < lenProgression) {
-      if (count === idx) {
-        question = `${question}.. `;
-      } else {
-        question = `${question}${getItemOfProgression(start, step, count)} `;
-      }
-      count += 1;
-    }
+    const question = getProgression(lenProgression, start, step, idx);
     const answer = getItemOfProgression(start, step, idx).toString();
     return cons(`Qustion: ${question}`, answer);
   });
